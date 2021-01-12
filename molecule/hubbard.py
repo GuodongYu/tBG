2402d428648d15e93c35aad2cb034a0cd7d877b7
GuodongYu <yugd@live.cn>
2021-01-12 10:00:46 +0800

    correct total energy and split Hamilotinian to spin up and down parts

--- molecule/hubbard.py
+++ molecule/hubbard.py
@@ -7,75 +7,58 @@ import pickle
 import json
 from monty.json import jsanitize
 
-def hamiltonian_no_hubbard(hopping, Es_onsite, n_orb):
-    n_dim = 2*n_orb # 2 for spin
-    H = np.zeros([n_dim, n_dim])
-    pairs, ts = hopping
-    ## hopping
-    def put_value_hopping(i):
-        ind0, ind1 = pairs[i]
-        H[2*ind0:2*(ind0+1),2*ind1:2*(ind1+1)] = ts[i]*np.identity(2)
-        H[2*ind1:2*(ind1+1),2*ind0:2*(ind0+1)] = np.transpose(ts[i]*np.identity(2))
-    [put_value_hopping(i) for i in range(len(pairs))]
-    ## onsite energy 
-    if Es_onsite != 0.:
-        np.fill_diagonal(H, np.column_stack([Es_onsite, Es_onsite]).reshape(1,-1))    
-    return H
-
 def hubbard_hamiltonian_MF(H_no_Hubbard, ns_up, ns_dn, U):
-    H = copy.deepcopy(H_no_Hubbard)
-    n_orb = len(ns_up)
-    n_dim = 2*n_orb
-    ## hubbard term
-    inds_up = np.array([2*i for i in range(n_orb)])
-    inds_dn = np.array([2*i+1 for i in range(n_orb)])
-    H[inds_up,inds_up] = H[inds_up,inds_up] + U*ns_dn
-    H[inds_dn,inds_dn] = H[inds_dn,inds_dn] + U*ns_up
-    C = U*np.dot(ns_up, ns_dn)
-    H = H -C*np.identity(n_dim) 
+    """
+    construct hamiltonian for both spin up and down parts
+    """ 
+    n_orb = H_no_Hubbard.shape[0]
+    ns = [ns_up, ns_dn]
+    H = []
+    for i in [0, 1]:
+        Hi = copy.deepcopy(H_no_Hubbard)
+        Hi = Hi + U*ns[1-i]*np.identity(n_orb)
+        H.append(Hi)
     return H
 
-def diag_hamiltonian(H_mat):
-    vals, vecs, info = zheev(H_mat, 1)
-    if info:
-        raise ValueError('lapack in scipy zheev failed!')
+def diag_hamiltonian(H):
+    vals = []
+    vecs = []
+    for i in [0, 1]:
+        val, vec, info = zheev(H[i], 1)
+        if info:
+            raise ValueError('lapack in scipy zheev failed!')
+        vals.append(val)
+        vecs.append(vec)
     return vals, vecs
 
 def spin_distribution_from_eigenvecs(vals, vecs, T):
-    n_orb = int(len(vals)/2)
-    chgs = np.abs(vecs)**2
+    n_orb = len(vals[0])
+    chgs = [np.abs(vecs[i])**2 for i in [0,1]]
+    vals_merge = np.concatenate(vals, axis=0)
+    ids_arg = np.argsort(vals_merge) 
+    occup = occup_TK(vals_merge[ids_arg], T=T, spin=2)
+    ids_up = np.where(ids_arg<n_orb)[0]
+    ids_dn = np.where(ids_arg>=n_orb)[0]
+    ns_up = np.sum([occup[ids_up][i]*chgs[0][:,i] for i in range(n_orb)], axis=0)
+    ns_dn = np.sum([occup[ids_dn][i]*chgs[1][:,i] for i in range(n_orb)], axis=0)
+    return ns_up, ns_dn
+
+def total_band_energy(vals, T, Ec):
+    """
+    Ec is the energy from the constant term U np.dot(ns_up, ns_dn)
+    """
+    vals = np.sort(np.concatenate(vals))
     occup = occup_TK(vals, T=T, spin=2)
-    inds_up = np.array([2*i for i in range(n_orb)])
-    inds_dn = np.array([2*i+1 for i in range(n_orb)])
-
-
-    ## deal with levels totally occupied 
-    inds_f1 = np.where(occup==1.0)[0] 
-    n_up_f1 = np.sum(chgs[:,inds_f1][inds_up,:], axis=1)
-    n_dn_f1 = np.sum(chgs[:,inds_f1][inds_dn,:], axis=1)
-    
-    ## deal with levels on fermi level
-    inds_ef = np.intersect1d(np.where(occup<1.0)[0], np.where(occup>0.0)[0])
-    if len(inds_ef):
-        f_ef = occup[inds_ef[0]]
-        n_up_ef = np.sum(chgs[:,inds_ef][inds_up,:], axis=1)*f_ef
-        n_dn_ef = np.sum(chgs[:,inds_ef][inds_dn,:], axis=1)*f_ef
-        return n_up_f1+n_up_ef, n_dn_f1+n_dn_ef
-    else:
-        return n_up_f1, n_dn_f1
-
-def total_band_energy(vals):
-    occup = occup_0K(vals, spin=2)
-    inds = np.where(occup>0.)[0]
-    return np.dot(vals[inds], occup[inds])
+    return np.dot(vals, occup)-Ec
 
 
 def calc_spin_distribution(H_no_hubbard, n_up_in, n_dn_in, U, T):
     n_orb = len(n_up_in)
     H_mat = hubbard_hamiltonian_MF(H_no_hubbard, n_up_in, n_dn_in, U)
     vals, vecs = diag_hamiltonian(H_mat)
-    e_tot = total_band_energy(vals)
     n_up_out, n_dn_out = spin_distribution_from_eigenvecs(vals, vecs, T)
+    Ec = U*np.dot(n_up_out, n_dn_out)
+    e_tot = total_band_energy(vals, T, Ec)
     return n_up_out, n_dn_out, e_tot
 
 def init_spin_distribution(qd, spin_sides=[1,1,1,1,1,1,1,1,1,1,1,1]):
@@ -115,37 +98,52 @@ def calc_diff_spin(n_up0, n_dn0, n_up1, n_dn1):
     diff_max = max(max_up, max_dn)
     return diff_max
     
-def calc_spin_dist_iteration(qd, U, spin_sides=[1,1,1,1,1,1,1,1,1,1,1,1], mix=0.8, prec=1.e-4, T=5, fout='profile'):
-    hopping = qd.hopping
-    Es_onsite = qd.Es_onsite
-    n_orb = len(qd.coords)
-    H_no_hubbard = hamiltonian_no_hubbard(hopping, Es_onsite, n_orb)
-    n_up0, n_dn0 = init_spin_distribution(qd, spin_sides)
-    n_up1, n_dn1, e_tot1 = calc_spin_distribution(H_no_hubbard, n_up0, n_dn0, U, T)
+
+def calc_spin_dist_iteration(qd, U, spin_sides=[1,1,1,1,1,1,1,1,1,1,1,1], mix0=0.8, mix1=0.1, prec=1.e-4, nmax=700, T=5, fout='profile'):
+    if mix0+mix1>1.0:
+        raise ValueError('mix0 + mix1 should be less than 1.0')
+    ###
     with open(fout, 'a') as f:
         f.write('Going into iteration...\n')
-        f.write('%4s %15s %15s %18s\n' %  ('','Diff_spin', 'Diff_etot', 'Etot'))
-        
-    i = 1
-    while True:
-        n_up_in = mix*n_up0 + (1-mix)*n_up1
-        n_dn_in = mix*n_dn0 + (1-mix)*n_dn1
-        n_up_out, n_dn_out, e_tot_out = calc_spin_distribution(H_no_hubbard, n_up_in, n_dn_in, U, T)
-            
-        diff_spin = abs(max(np.abs(n_up_in-n_dn_in))- max(np.abs((n_up_out- n_dn_out))))
-        diff_etot = e_tot_out - e_tot1
+        f.write('%4s %15s %18s\n' %  ('', 'Diff_etot', 'Etot'))
+   
+    H_no_hubbard = qd.get_Hamiltonian() 
+    def init_prepare():    
+        n_up_in0, n_dn_in0 = init_spin_distribution(qd, spin_sides)
+        n_up_in1, n_dn_in1, e_tot_in1 = calc_spin_distribution(H_no_hubbard, n_up_in0, n_dn_in0, U, T)
         with open(fout, 'a') as f:
-            f.write('%4i %15f %15f  %18f\n' % (i, diff_spin, diff_etot, e_tot_out))
-        if abs(diff_etot)<=prec:
+            f.write('%4i %15f  %18f\n' % (0, e_tot_in1, e_tot_in1))
+
+        n_up_in2, n_dn_in2, e_tot_in2 = calc_spin_distribution(H_no_hubbard, n_up_in1, n_dn_in1, U, T)
+        with open(fout, 'a') as f:
+            f.write('%4i %15f  %18f\n' % (1, e_tot_in2-e_tot_in1, e_tot_in2))
+        return n_up_in0, n_up_in1, n_up_in2, n_dn_in0, n_dn_in1, n_dn_in2, e_tot_in2
+
+    n_up_in0, n_up_in1, n_up_in2, n_dn_in0, n_dn_in1, n_dn_in2, e_tot_in = init_prepare()
+    
+    i = 2
+    while True:
+        n_up_in = mix0*n_up_in0 + mix1*n_up_in1 + (1-mix0-mix1)*n_up_in2
+        n_dn_in = mix0*n_dn_in0 + mix1*n_dn_in1 + (1-mix0-mix1)*n_dn_in2
+        n_up_out, n_dn_out, e_tot_out = calc_spin_distribution(H_no_hubbard, n_up_in, n_dn_in, U, T)
+        with open(fout, 'a') as f:
+            f.write('%4i %15f  %18f\n' % (i, e_tot_out-e_tot_in, e_tot_out))
+        if abs(e_tot_out-e_tot_in)<=prec:
             with open(fout, 'a') as f:
-                f.write('Get converged!\n\n')
+                f.write('Got converged!\n')
+            converged = True
             break
         else:
-            n_up0, n_dn0 = n_up_in, n_dn_in
-            n_up1, n_dn1 = n_up_out, n_dn_out
-            e_tot1 = e_tot_out
+            n_up_in0, n_up_in1, n_up_in2 = n_up_in1, n_up_in2, n_up_out
+            n_dn_in0, n_dn_in1, n_dn_in2 = n_dn_in1, n_dn_in2, n_dn_out
+            e_tot_in = e_tot_out
             i = i + 1
-    return n_up_out, n_dn_out, e_tot_out
+            if i>nmax:
+                with open(fout, 'a') as f:
+                    f.write('EXIT Warning: exceed the max step number %s!!\n\n' % nmax)
+                converged = False
+                break
+    return n_up_out, n_dn_out, e_tot_out, converged
 
 def plot_spin_dist(qd, n_up, n_dn, scale=8000, alpha=0.5):
     qd.add_hopping_wannier(ts=[-2.8])
@@ -167,7 +165,7 @@ def size2R(size):
     R = size*1.5*b/np.cos(15*np.pi/180)
     return R
 
-def main(size, U, phase):
+def main(size, U, phase, P=0):
     fout = 'size_%s/profile_U%s' % (size, U)
     from tBG.molecule.structures import QuantumDotQC
     spin_sides = spin_sides_distribution(phase)
@@ -177,12 +175,12 @@ def main(size, U, phase):
     with open(fout, 'a') as f:
         f.write('num_electron: %s\n' % len(qc.coords))
         f.write('phase: %s\n' % phase)
-    qc.add_hopping_wannier(P=0)
-    n_up, n_dn, e_tot = calc_spin_dist_iteration(qc, U, spin_sides, mix=0.01, prec=1.e-4, T=5, fout=fout) 
+    qc.add_hopping_wannier(P=P)
+    n_up, n_dn, e_tot, converged = calc_spin_dist_iteration(qc, U, spin_sides, mix0=0.001,mix1=0.001, prec=1.e-4, T=10, nmax=500, fout=fout) 
     with open('size_%s/struct.obj' % size, 'wb') as f:
         pickle.dump(qc, f)
-    out = {'n_up':n_up, 'n_dn':n_dn, 'e_tot':e_tot}
-    with open('size_%s/U_%s_initPhase_%s.json' % (size, U, phase), 'w') as f:
+    out = {'n_up':n_up, 'n_dn':n_dn, 'e_tot':e_tot, 'converged':converged}
+    with open('size_%s/U_%s/initPhase_%s.json' % (size, U, phase), 'w') as f:
         json.dump(jsanitize(out), f) 
 
     #plot_spin_dist(qc, n_up, n_dn, scale=3000)
@@ -198,16 +196,15 @@ def spin_sides_distribution(phase='NM'):
         return [1,-1,1,-1,1,-1,1,-1,1,-1,1,-1]
         
 if __name__ == "__main__":
-    Rs = [2]
-    Us = [3]
-    sizes = [2]
-    phases = ['NM', 'AF-AF','FM-FM-FM','FM-FM-AF']
+    P = 20
+    Us = [2, 3]
+    sizes = [9]
+    phases = ['NM', 'AF-AF', 'FM-FM-FM', 'FM-FM-AF']
     for size in sizes:
-        os.mkdir('size_%s' % size)
+        if not os.path.isdir('size_%s'%size):
+            os.mkdir('size_%s' % size)
         for U in Us:
+            if not os.path.isdir('size_%s/U_%s'%(size, U)):
+                os.mkdir('size_%s/U_%s' % (size, U))
             for phase in phases:
-                main(size, U, phase)    
-
-
-    
-    
+                main(size, U, phase, P=P)    
